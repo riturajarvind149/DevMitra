20cd3e57a002858586919c822c21e57ed4224695
Ritu RAj Arvind <ritu.p25@medhaviskillsuniversity.edu.in>
2026-06-10 02:30:43 +0530

    Setup backend, PostgreSQL and Prisma

---
+++ backend/prisma/migrations/20260609205600_create_user_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "githubId" TEXT,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatarUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
